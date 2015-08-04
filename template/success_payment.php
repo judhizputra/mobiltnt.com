c6d66f8a47a338d230600ef1adb93588e70bfd01
judhizputra <yudhistira.ep@gmail.com>
2015-08-04 10:12:50 +0700

    Finishing Success Payment Page
    Mobile ready, reading to coding

---
+++ template/success_payment.php
@@ -0,0 +1,34 @@
+		<?php include('header.php'); ?>
+        
+        <div class="gap"></div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <i class="fa fa-check round box-icon-large box-icon-center box-icon-success mb30"></i>	
+                    <h2 class="text-center">Pembayaran Anda telah Kami terima!</h2>
+                    <h5 class="text-center mb30">Detail order <strong>#TNT20150801001</strong> akan dikirim ke johndoe@gmail.com</h5>
+                    <ul class="order-payment-list list mb30">
+                        <li>
+                            <div class="row">
+                                <div class="col-xs-8">
+                                    <h5><i class="fa fa-car"></i> Rental mobil selama 5 hari</h5>
+                                    <p><small>1 Agustus 2015 <i class="fa fa-arrow-right booking-item-payment-date-separator"></i> 5 Agustus 2015</small>
+                                    </p>
+                                </div>
+                                <div class="col-xs-4">
+                                    <p class="text-right"><span class="text-lg"><strong>Rp 2.860.000</strong></span>
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="text-center">
+                    	<a href="index.php" class="btn btn-danger">Kembali ke halaman utama</a>
+                    </div>
+                </div>
+            </div>
+            <div class="gap"></div>
+        </div>
+        
+        <?php include('footer.php'); ?>
