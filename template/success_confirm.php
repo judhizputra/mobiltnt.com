553183510f8273d04944c14d7e79bb46d933c957
judhizputra <yudhistira.ep@gmail.com>
2015-08-04 16:33:07 +0700

    Finishing Success Register & Confirm
    - Mobile ready & ready to coding

---
+++ template/success_confirm.php
@@ -0,0 +1,19 @@
+		<?php include('header.php'); ?>
+        
+        <div class="gap"></div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <i class="fa fa-check round box-icon-large box-icon-center box-icon-success mb30"></i>	
+                    <h2 class="text-center">Selamat bergabung di mobiltnt.com!</h2>
+                    <h5 class="text-center mb30">Anda akan di arahkan secara otomatis ke halaman utama dalam 5 detik, jika halaman tidak terbuka Anda dapat meng-klik tombol di bawah ini.</h5>
+                    <div class="text-center">
+                    	<a href="index.php" class="btn btn-danger">Kembali ke halaman utama</a>
+                    </div>
+                </div>
+            </div>
+            <div class="gap"></div>
+        </div>
+        
+        <?php include('footer.php'); ?>
---
+++ template/success_register.php
@@ -0,0 +1,19 @@
+		<?php include('header.php'); ?>
+        
+        <div class="gap"></div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <i class="fa fa-check round box-icon-large box-icon-center box-icon-success mb30"></i>	
+                    <h2 class="text-center">Data berhasil disimpan, cek email Anda!</h2>
+                    <h5 class="text-center mb30">Kami sudah mengirimkan email ke johndoe@gmail.com untuk verifikasi akun Anda. Klik tombol dibawah jika Anda tidak menerima email dari Kami.</h5>
+                    <div class="text-center">
+                    	<a href="index.php" class="btn btn-danger">Kirim ulang verifikasi</a>
+                    </div>
+                </div>
+            </div>
+            <div class="gap"></div>
+        </div>
+        
+        <?php include('footer.php'); ?>
