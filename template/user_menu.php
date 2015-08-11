2d992ee14297410ce9c4aca88b1a4e9ceaa8fc62
judhizputra <yudhistira.ep@gmail.com>
2015-08-11 15:35:39 +0700

    Change menu item position
    Change menu item position

--- template/user_menu.php
+++ template/user_menu.php
@@ -8,9 +8,9 @@
                             <li><a href="user_dashboard.php"><i class="fa fa-user"></i>Dashboard</a></li>
                             <li><a href="user_setting.php"><i class="fa fa-cog"></i>Pengaturan</a></li>
                             <li><a href="user_order_history.php"><i class="fa fa-clock-o"></i>Riwayat Pesanan</a></li>
-                            <li><a href="user_rent_history.php"><i class="fa fa-history"></i>Riwayat Rental</a></li>
                             <li><a href="user_rent_setting.php"><i class="fa fa-home"></i>Pengaturan Rental</a></li>
                             <li><a href="user_rent_list.php"><i class="fa fa-car"></i>Daftar Mobil Rental</a></li>
+                            <li><a href="user_rent_history.php"><i class="fa fa-history"></i>Riwayat Rental</a></li>
                         </ul>
                     </aside>
                 </div>
