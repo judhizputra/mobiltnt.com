17c3fac9da2b78883b5803b1b8a6ad0d173294cc
judhiz <yudhistira.eka.putra@gmail.com>
2015-09-09 15:42:28 +0700

    Terms & Condition Page
    Terms & Condition Page

---
+++ template/terms_condition.php
@@ -0,0 +1,61 @@
+		<?php include('header.php'); ?>
+
+        <div class="container">
+            <h1 class="page-title">Syarat & Ketentuan</h1>
+        </div>
+
+        <div class="container">
+            <div class="gap-small"></div>
+            <p>Syarat dan Ketentuan penggunaan <a href="http://www.mobiltnt.com/">MobilTNT.com</a> (beserta dokumen yang disebut di dalamnya) menundukkan Anda pada syarat-syarat penggunaan yang menjadi dasar bagi Anda dalam penggunaan laman situs kami <a href="http://www.mobiltnt.com/">MobilTNT.com</a>, baik sebagai tamu atau pengguna terdaftar. Anda setuju bahwa penggunaan laman situs ini secara terus-menerus oleh Anda menunjukkan bahwa Anda menerima dan menyetujui syarat-syarat penggunaan ini dan Anda sepakat untuk mematuhinya. Apabila Anda tidak setuju atas syarat-syarat penggunaan ini, kami mohon untuk tidak melanjutkan penggunaan situs kami.</p>
+            <p>Informasi mengenai kami <a href="http://www.mobiltnt.com/">MobilTNT.com</a> adalah situs yang dioperasikan dan dimiliki oleh PT Anugerah Gelindo ("Kami"), suatu perseroan terbatas yang didirikan berdasarkan hukum Republik Indonesia. Setiap pertanyaan mengenai kami atau situs web kami dapat dikirim ke <a href="mailto:support@mobiltnt.com">support[at]mobiltnt[dot]com</a>.<p>
+            <p class="lead noMargin">Section 1: Bergabung menjadi anggota Kami</p>
+            <ol>
+                <li>Setiap pengguna wajib untuk menjadi anggota Kami dan memberikan informasi di bawah ini untuk dapat melakukan penyewaan:
+                    <ul>
+                        <li>Nama asli (bukan alias ataupun nama fiktif)</li>
+                        <li>Alamat surat elektronik (email)</li>
+                        <li>Nomor telpon yang dapat dihubungi</li>
+                        <li>Alamat tempat tinggal atau alamat penjemputan</li>
+                    </ul>
+                </li>
+                <li>Setiap rental wajib untuk menjadi anggota Kami dan memberikan informasi di bawah ini untuk dapat mengiklankan mobilnya:
+                    <ul>
+                        <li>Nama usaha asli (bukan alias ataupun nama fiktif)</li>
+                        <li>Alamat surat elektronik (email)</li>
+                        <li>Nomor telpon yang dapat dihubungi</li>
+                        <li>Domisili usaha</li>
+                        <li>Contact Person yang dapat dihubungi</li>
+                    </ul>
+                </li>
+                <li>Setiap anggota wajib menjaga kerahasiaan kata kunci (password) yang digunakan untuk mengakses akun <a href="http://www.mobiltnt.com/">MobilTNT.com</a>. Bila tidak ada pemberitahuan dari Anda, Kami akan beranggapan bahwa setiap orang yang menggunakan akun Anda telah memperoleh izin dari Anda.</li>
+                <li>Kami berhak melakukan verifikasi terhadap informasi yang Anda berikan. Kami berhak pula untuk membatasi penggunaan situs hingga proses verifikasi selesai.</li>
+            </ol>
+            
+            <p class="lead noMargin">Section 2: Melakukan penyewaan</p>
+            <ol>
+                <li>Semua aktifitas penyewaan dilakukan melalui situs Kami.</li>
+                <li>Pemesanan valid bila pembayaran sudah dilakukan, jika dalam 1x12 jam Anda tidak melakukan pembayaran, pesanan Anda akan di batalkan secara otomatis.</li>
+                <li>Pastikan mobil yang akan Anda rental sudah sesuai dengan kebutuhan, jika mobil yang Anda rental tidak sesuai dengan yang diiklankan pada <a href="http://www.mobiltnt.com/">MobilTNT.com</a> Anda dapat menolak/membatalkan pesanan.</li>
+            </ol>
+            
+            <p class="lead noMargin">Section 3: Pembatalan pesanan</p>
+            <ol>
+                <li>Dengan memesan melalui Website Kami, Anda ikut ke dalam perjanjian dengan <a href="http://www.mobiltnt.com/">MobilTNT.com</a> dalam hal persetujuan dengan rental yang berpartisipasi. Rental bertanggung jawab untuk persiapan dan kualitas mobil retal pesanan Anda. Anda setuju untuk memberikan perhatian khusus ketika memberikan Kami informasi Anda dan Anda menjamin bahwa informasi tersebut akurat dan lengkap saat memesan.</li>
+                <li>Anda harus segera memberitahu <a href="http://www.mobiltnt.com/">MobilTNT.com</a> jika Anda memutuskan untuk membatalkan pesanan Anda, sebaiknya via telepon, dan Anda memberikan nomor pesanan Anda. Jika rental setuju untuk menerima pembatalan, dan tidak ada biaya pembatalan. Namun jika rental menolak pembatalan, contohnya : mobil sudah disiapkan khusus untuk Anda, itu tidak dapat dibatalkan atau dapat dibatalkan sesuai dengan ketentuan yang diberikan oleh rental.</li>
+            </ol>
+            
+            <p class="lead noMargin">Section 4: Tindak tanduk pengguna</p>
+            <ol>
+                <li>Anda setuju untuk menggunakan situs Kami sesuai dengan Syarat dan Ketentuan serta tidak melanggar hukum dan peraturan yang berlaku.</li>
+                <li>Anda setuju untuk tidak melakukan peniruan, menjual, ataupun penyalahgunaan laman situs Kami untuk segala keperluan komersial, kecuali bila diperbolehkan dalam Syarat dan Ketentuan yang berlaku.</li>
+                <li>Dengan menjadi anggota Kami, Anda menyetujui bahwa informasi Anda disimpan dalam database Kami termasuk di dalamnya Data Anda termasuk semua pesanan yang Anda lakukan melalui situs Kami.</li>
+                <li>Kami berhak untuk menolak melayani Anda, membatasi, menghentikan sementara, dan mencabut keanggotaan Anda kapan pun, dengan atau tanpa alasan.</li>
+                <li>Kami dapat mengirimkan informasi dan menawarkan produk dan/atau jasa pada Anda sebagai anggota <a href="http://www.mobiltnt.com/">MobilTNT.com</a> dari waktu ke waktu.</li>
+                <li>Kami tidak akan bertanggung jawab atas kegagalan atau keterlambatan dalam kinerja pelayanan atau pengiriman barang di mana kegagalan tersebut merupakan akibat dari tindakan atau kelalaian yang berada di luar kendali Kami, seperti kejadian-kejadian tak terduga yang disebabkan secara langsung dan eksklusif seperti kejadian alam yang tidak dapat diantisipasi, dikendalikan atau dicegah dengan pelaksanaan yang bijaksana,ketekunan dan ketelitian, termasuk pada: perang, kerusuhan, huru-hara; kepatuhan kepada hukum atau peraturan pemerintah, aturan, regulasi atau arahan dan tindakan dari pihak ketiga.</li>
+                <li>Apabila <a href="http://www.mobiltnt.com/">MobilTNT.com</a> memiliki keyakinan yang beralasan dimana terdapat penyalahgunaan dalam penggunaan voucher dan/atau kode diskon atau diduga adanya kasus penipuan, <a href="http://www.mobiltnt.com/">MobilTNT.com</a> diperkenankan pembeli (atau customer) untuk segera di blokir dan berhak untuk menolak semua layanan di kemudian hari. Selain itu, jika ada penyalahgunaan kupon atau kode diskon, <a href="http://www.mobiltnt.com/">MobilTNT.com</a> berhak untuk meminta kompensasi dari salah satu atau semua pelanggar.</li>
+                <li>Penawaran yang diberikan merupakan kebijaksanaan dari <a href="http://www.mobiltnt.com/">MobilTNT.com</a> dan dapat setiap saat berubah atau di tarik kembali tanpa pemberitahuan terlebih dahulu.</li>
+            </ol>
+            <div class="gap"></div>
+        </div>
+        
+        <?php include('footer.php'); ?>
