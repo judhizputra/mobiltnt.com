5655c0f5c9398e3631aacdf6f71be0c05d120054
judhiz <yudhistira.eka.putra@gmail.com>
2015-09-09 10:32:59 +0700

    About Page
    About Page

---
+++ template/about_us.php
@@ -0,0 +1,49 @@
+		<?php include('header.php'); ?>
+
+        <div class="container">
+            <h1 class="page-title">Tentang Kami</h1>
+        </div>
+
+        <div class="container">
+            <div class="gap-small"></div>
+            <ul class="nav nav-tabs tabs">
+                <li class="active"><a href="#us" data-toggle="tab">Tentang MobilTNT</a></li>
+                <li><a href="#prokit" data-toggle="tab">Pesan dari CEO MobilTNT.com, Ricky Conrad</a></li>
+            </ul>
+            <div class="gap-small"></div>
+            <div class="tab-content tab_container" style="text-align:justify;">
+                <div class="tab-pane active" id="us">					
+                    <!--Content-->
+                    <div id="content_post"> 
+                        <p> <a href="http://www.mobiltnt.com/" style="font:Arial; font-size:18px; font-weight:bold; color:#d74c47;">MobilTNT.com</a> 
+                            merupakan marketplace sewa mobil terbesar di Indonesia yang membantu Anda mencari dan menemukan mobil sesuai kebutuhan Anda. </p>
+                        <p> Jika Anda mencari <a href="http://www.mobiltnt.com/rental-mobil-di-jakarta">rental mobil, sewa mobil</a> berdasarkan kota, <a href="http://www.mobiltnt.com/rental-mobil-di-bali">rental mobil lepas kunci, sewa mobil lepas kunci</a>, <a href="http://www.mobiltnt.com/rental-mobil-di-bandung">rental mobil dengan supir, sewa mobil dengan supir</a>, <a href="http://www.mobiltnt.com/">MobilTNT.com</a> memiliki database yang besar dan user friendly, sehingga kami berharap dapat membuat Anda tersenyum karena mendapat mobil terbaik. </p>
+                        <p> <a href="http://www.mobiltnt.com/">MobilTNT.com</a> didukung team IT terbaik, kami berupaya untuk selalu memberikan informasi terkini dan terlengkap seputar rental mobil yang ada di seluruh Indonesia. <a href="http://www.mobiltnt.com/">MobilTNT.com</a> memiliki berbagai promo menarik yang dapat Anda dapatkan jika Anda men-subscribe newsletter, juga bisa melalui <a href="https://www.facebook.com/mobiltnt">facebook</a>, <a href="https://www.facebook.com/groups/mobiltnt/">facebook group</a>, <a href="https://www.facebook.com/mobiltntcom">facebook fans page</a> dan <a href="https://twitter.com/mobiltnt">twitter</a>.</p>
+                    </div>
+                    <div class="gap"></div>
+                    <h4>HOW WE MAKE MONEY</h4>
+                    <p> <a href="http://www.mobiltnt.com/">MobilTNT.com</a> menciptakan pendapatan dari komisi transaksi yang terjadi. Caranya; Kami menciptakan situs online terbaik untuk mencari rental mobil, pe-rental (perusahaan atau pemilik langsung) mengiklankan mobilnya di <a href="http://www.mobiltnt.com/">MobilTNT.com</a>, Anda, dan keluarga Anda menemukan mobil retal melalui <a href="http://www.mobiltnt.com/">MobilTNT.com</a>, kami (bersama Anda) senantiasa berinovasi membuat <a href="http://www.mobiltnt.com/">MobilTNT.com</a> lebih baik dan lebih baik. Simple bukan? </p>
+                </div>
+                <div class="tab-pane" id="prokit">
+                    <div class="media">
+                        <div class="pull-left">
+                            <img src="http://www.mobiltnt.com/assets/img/ricky.jpg" class="media-object thumbnail" />
+                        </div>
+                        <div class="media-body">
+                            <h3>Para Pengguna Internet/Pemilik Rental Yang Terhormat,</h3>
+                            <p> Sejalan dengan misi ke depan <a href="http://www.mobiltnt.com/">MobilTNT.com</a> untuk menjadi <em>marketplace</em> yang berkancah di Indonesia di awal tahun 2015 dan guna meningkatkan kenyamanan dan pelayanan kami kepada para pengguna internet khususnya kepada para Pemilik Rental sebagai <em>marketplace</em> terkemuka di Indonesia, kami telah membuat lay out media kami menjadi lebih representatif, menarik dan efektif. Dengan bahasa dan petunjuk yang mudah dimengerti serta dilengkapi dengan link social media seperti facebook, Twitter dan Google+ yang semuanya dikemas dengan penyajian yang lebih komunikatif, terkini, edukatif dan informatif, diharapkan media <a href="http://www.mobiltnt.com/">MobilTNT.com</a> selalu menjadi <em>marketplace</em> no.1 yang dicari bagi para pencari mobil rental dan juga selalu menjadi <em>marketplace</em> partner no.1 bagi para Pemilik Rental baik dalam mempromosikan/memasarkan mobil-mobil rental mereka secara online maupun sebagai media untuk memuat berita/event bergengsi mereka. </p>
+                            <p> Modifikasi yang telah kami lakukan dengan keyakinan bahwa para pengguna internet baik yang hendak mencari mobil rental maupun yang hendak mempromosikan/memasarkan mobil-mobil mereka selalu menjadikan <em>marketplace</em> <a href="http://www.mobiltnt.com/">MobilTNT.com</a> pilihan no.1 dalam mendapatkan apa yang mereka inginkan setiap saat dan dimana saja mereka mengakses <a href="http://www.mobiltnt.com/">MobilTNT.com</a>.</p>
+                            <p> Kami sadar kami jauh dari sempurna, kiranya kami dapat terus meningkatkan kinerja <a href="http://www.mobiltnt.com/">MobilTNT.com</a> agar dapat membuat Anda tersenyum dan kembali mengunjungi situs ini. Saran dan kritik Anda sangat kami harapkan demi mencapai cita2 kami. GO ONLINE! </p>
+                            <p>Salam Hangat,</p>
+                            <img src="http://www.mobiltnt.com/assets/img/sign.png" style="width:100px;"/><br />
+                            Ricky Conrad<br />
+                            Chief Executive Officer MobilTNT.com<br />
+                            <a href="mailto:ceo@propertykita.com">ceo[at]mobiltnt[dot]com</a><br />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="gap"></div>
+        </div>
+        
+        <?php include('footer.php'); ?>
