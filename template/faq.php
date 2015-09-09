5677cd965a1874ea77daa9397ad21b353c179102
judhiz <yudhistira.eka.putra@gmail.com>
2015-09-09 11:54:46 +0700

    FAQ Page
    FAQ Page

---
+++ template/faq.php
@@ -0,0 +1,61 @@
+		<?php include('header.php'); ?>
+
+        <div class="container">
+            <h1 class="page-title">FAQ</h1>
+        </div>
+
+        <div class="container">
+            <div class="gap-small"></div>
+            <p class="lead">Apa itu <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p><a href="http://www.mobiltnt.com/">MobilTNT.com</a> adalah situs penyewaan mobil terbesar di Indonesia. Kami menyediakan beragam mobil yang berasal dari ratusan partner rental kami disamping inventori mobil kami sendiri dengan tujuan memudahkan anda dan memberikan solusi sewa mobil yang terbaik. Kami menyediakan transaksi sewa yang aman karena kami di <a href="http://www.mobiltnt.com/">MobilTNT.com</a> menjamin setiap transaksi anda sesuai ketentuan yang berlaku.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Siapa Saja yang dapat menjadi Pengguna <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p>Pengguna <a href="http://www.mobiltnt.com/">MobilTNT.com</a> adalah pengguna dari segmen ritel, korporasi, dan agensi yang terkoneksi ke jaringan internet. Pada segmen korporasi dan agensi dapat melakukan pembayaran di belakang dan menikmati fitur seperti fixed price dan insentive. Untuk lebih jelasnya mengenai pengguna korporasi dan agensi silahkan hubungi <a href="mailto:support@mobiltnt.com">support[at]mobiltnt[dot]com</a>.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Bagaimana cara memesan mobil di <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p>Pemesanan bisa dilakukan lewat website <a href="http://www.mobiltnt.com/">www.mobiltnt.com</a>. Prosedurnya sangat sederhana: Pilih lokasi penjemputan, kemudian pilih rentang waktu penyewaan, lalu pilih ingin dengan supir atau lepas kunci kemudian klik cari. Setelah memilih mobil, pilih opsi lainya seperti dengan gps, tempat duduk bayi atau bahan bakar. <a href="http://www.mobiltnt.com/">MobilTNT.com</a> akan memberikan solusi yang terbaik berdasarkan pilihan anda. </p>
+            <p>Ketika anda menentukan pilihan anda dan melakukan booking, sistem kami akan mengkonfirmasi pesanan anda dan kami langsung berkoordinasi dengan rental penyedia mobil, anda akan mendapatkan email konfirmasi untuk melakukan pembayaran, mudah bukan?.</p>
+            <p>Kesimpulan sederhana : menyewa mobil sekarang bukan pekerjaan sulit lagi bukan?</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Bagaimana cara pembayaran <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p>Pembayaran di Indoloka dapat menggunakan metode transfer, internet banking maupun kartu kredit. Nantinya akan juga tersedia fasilitas pembayaran dengan menggunakan uang cash ke Indomaret maupun Alfamart.</p>
+            <p>Yang anda bayarkan hanya pembayaran untuk hari pertama dan untuk hari berikutnya anda dapat membayarkan ke pengemudi secara langsung. Harga yang kami berikan belum termasuk tips dan permintaan khusus lainnya. Untuk hal tersebut dapat dinegosiasikan dengan supir ditempat.</p>
+            <p>Untuk pengguna korporasi dan segmen perusahaan, pembayaran dapat dilakukan dibelakang hari. Kami memberikan kredit sampai dengan 2 minggu untuk partner atau perusahaan yang memenuhi kualifikasi.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Bagaimana Kebijakan Refund <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p>Refund hanya diberikan jika terbukti rental mobil tidak memberikan layanannya atau layanan tidak sesuai dengan yang dijanjikan. Untuk layanan yang tidak sesuai dengan yang dijanjikan, <a href="http://www.mobiltnt.com/">MobilTNT.com</a>  menfasilitasi dispute secara manual dan untuk layanan yang tidak tersedia, <a href="http://www.mobiltnt.com/">MobilTNT.com</a>  menjamin uang kembali.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Apa saja mobil yang tersedia?</p>
+            <p>Mobil yang tersedia saat ini adalah mobil - mobil yang umum disewa seperti avanza, kijang, hingga alphard di kota kota tertentu. Kami saat ini belum menyediakan limosine service ataupun mobil - mobil mewah khusus.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Berapa lama booking dilakukan sebelum tanggal penyewaan?</p>
+            <p>Pada dasarnya kapan saja, tapi mengingat banyak rental mobil dinamis, kami sangat merekomendasikan tidak lebih dari 1 bulan dimuka untuk booking sewa mobil.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Apa saja opsi yang tersedia dalam sewa mobil di <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p>Semua sewa mobil di <a href="http://www.mobiltnt.com/">MobilTNT.com</a> adalah sewa dengan supir maupun lepas kunci. Jumlah hari tidak dibatasi, boleh sehari ataupun sebulan.</p>
+            <p>Setiap rental mobil memiliki kebijakan yang berbeda - beda, jadi mohon diperhatikan apakah fiturnya sudah sesuai dengan kebutuhan Anda, jangan sekedar melihat harga yang murah saja. Fasilitas seperti AC, ban cadangan dan lainnya perlu menjadi perhatian dari pengguna.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Di daerah mana saja saya bisa memesan mobil melalui website <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p>Saat ini kami menyediakan layanan kami di 90 kota besar dan sekitarnya di Indonesia dan akan terus bertambah dari waktu ke waktu. Inilah kota yang kami layani: Bali, Balikpapan, Banda Aceh, Bandar Lampung, Bandung, Banjarmasin, Batam, Jakarta, Lombok, Makassar, Malang, Manado, Medan, Padang, Palembang, Pangkal Pinang, Pekanbaru, Pontianak, Purwokerto, Samarinda, Semarang, Solo, Surabaya, Yogyakarta, Ternate, Tegal, Ambon, Banjar Baru, Batu, Bekasi, Bengkulu, Blitar, Bogor, Bontang, Bukittinggi, Cilegon, Cirebon, Depok, Gorontalo, Jambi, Jayapura, Kediri, Kupang, Madiun, Magelang, Mataram, Mojokerto, Palangka Raya, Palopo, Palu, Pasuruan, Pekalongan, Pematang Siantar, Salatiga, Serang, Singkawang, Sorong, Sukabumi, Tangerang, Tangerang Selatan, Tasikmalaya, Bangka, Martapura, Kudus, Poso, Luwuk, Banjanegara, Bantul, Banyuwangi, Banyumas, Belitung, Cianjur, Ciamis, Cilacap, Garut, Jepara, Jombang, Lamongan, Lumajang, Manokwari, Nganjuk, Pacitan, Ponorogo, Purbalingga, Sidoarjo, Sleman, Sumbawa, Sumedang, Tulungagung, Kendari.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Apakah saya bisa memasarkan mobil-mobil rental saya di <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p>Tentu, anda dapat memasarkan rental anda melalui website kami, silahkan daftar melalui KLIK  <a href="http://www.mobiltnt.com/auth">link ini</a>. Ikuti step-step yang tampil untuk registrasi, setelah akun rental anda aktif, anda dapat menambah mobil melalui halaman admin akun anda dan mobil-mobil dari rental anda akan bisa dicari dan diorder customer melalui website kami.</p>
+            
+            <div class="gap"></div>
+            <p class="lead">Apakah ada biaya keanggotaan rental di <a href="http://www.mobiltnt.com/">MobilTNT.com</a>?</p>
+            <p><a href="http://www.mobiltnt.com/">MobilTNT.com</a> 100% gratis, tidak ada biaya tersembunyi atau sejenisnya, anda cukup mendaftar saja melalu website kami dan mengisi detail rental dan mobil mobil yang tersedia di rental anda.</p>
+            <p>Untuk pertanyaan lebih lanjut silahkan hubungi kami melalui telpon di (021) 294 72 888 atau email di <a href="mailto:support@mobiltnt.com">support[at]mobiltnt[dot]com</a>.</p>
+            
+            <div class="gap"></div>
+        </div>
+        
+        <?php include('footer.php'); ?>
