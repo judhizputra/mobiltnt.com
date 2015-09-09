6419b65c781e15883382f20b206cc27edde1a7f5
judhiz <yudhistira.eka.putra@gmail.com>
2015-09-09 16:29:28 +0700

    Success Order Page
    Success Order Page

---
+++ template/success_order.php
@@ -0,0 +1,65 @@
+		<?php include('header.php'); ?>
+        
+        <div class="gap"></div>
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <i class="fa fa-check round box-icon-large box-icon-center box-icon-success mb30"></i>	
+                    <h2 class="text-center">Pesanan Anda telah Kami terima!</h2>
+                    <h5 class="text-center mb30">Detail order <strong>#TNT20150801001</strong> akan dikirim ke johndoe@gmail.com</h5>
+                    <ul class="order-payment-list list mb30">
+                        <li>
+                            <div class="row">
+                                <div class="col-xs-8">
+                                    <h5><i class="fa fa-car"></i> Rental mobil selama 5 hari</h5>
+                                    <p><small>1 Agustus 2015 <i class="fa fa-arrow-right booking-item-payment-date-separator"></i> 5 Agustus 2015</small>
+                                    </p>
+                                </div>
+                                <div class="col-xs-4">
+                                    <p class="text-right"><span class="text-lg"><strong>Rp 2.860.000</strong></span>
+                                    </p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                    
+                    <h5 class="text-center"><strong>Cara Pembayaran Bank dan Rekening Tujuan</strong></h2>
+                    <p>Silakan masukkan nomor invoice Anda dalam BERITA pada saat Anda melakukan pembayaran melalui ATM Non-Tunai, setoran Bank, atau Internet Banking. Anda dapat melakukan pembayaran ke nomor rekening berikut:</p>
+                    
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="card-thumb mb30">
+                                        <p>Bank BCA Kelapa Gading</p>
+                                        <p class="card-thumb-number">XXXX XXX XXXX XXXX</p>
+                                        <img title="Logo Bank BCA" alt="Logo Bank BCA" src="img/logo-bca.png" class="card-thumb-type img-responsive">
+                                        <small>atas nama</small>
+                                        <h5>PT. Anugerah Gelindo</h5>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="card-thumb">
+                                        <p>Bank Mandiri Kelapa Gading</p>
+                                        <p class="card-thumb-number">XXXX XXX XXXX XXXX</p>
+                                        <img title="Logo Bank BCA" alt="Logo Bank BCA" src="img/logo-mandiri.png" class="card-thumb-type img-responsive">
+                                        <small>atas nama</small>
+                                        <h5>PT. Anugerah Gelindo</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="gap-small"></div>
+                    
+                    <div class="text-center">
+                    	<a href="index.php" class="btn btn-danger">Kembali ke halaman utama</a>
+                    </div>
+                </div>
+            </div>
+            <div class="gap"></div>
+        </div>
+        
+        <?php include('footer.php'); ?>
