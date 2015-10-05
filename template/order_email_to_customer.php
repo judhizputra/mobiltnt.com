09ec4d3ca60df18e397fe04eaae8fe3f6db4d255
judhiz <yudhistira.eka.putra@gmail.com>
2015-10-05 15:31:33 +0700

    Order Email to Customer
    Order Email to Customer

---
+++ template/order_email_to_customer.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MobilTNT User Activation</title>
+</head>
+<body style="background-color:#333333; padding:75px 0 50px; color:#4d4d4d; font-family: arial, \'sans serif\'; font-size:12px;">
+<div style="width:550px; margin:0 auto; border-radius:3px;">
+  <div>
+    <div style="height:40px; background-color:#000; text-align:center; padding:20px;">
+      <img src="http://www.mobiltnt.com/assets/img/logo.png"/>
+    </div>
+  </div>
+  <div style="background-color:#fff; margin-bottom:1px; padding:20px;">
+    <p style="margin:0 0 10px;">Hi Pelanggan! </p>
+    <p> Berikut adalah rincian dari pesanan Anda.</p>
+    <p style="margin:0 0 10px; font-size:18px;">No Order TNT201510JU2MN</p>
+    <table style="width:100%;">
+      <thead>
+        <tr>
+          <th style="text-align:left; white-space:nowrap;" width="30%">Deskripsi</th>
+          <th style="text-align:left; white-space:nowrap; text-align:right;" width="25%">Harga</th>
+          <th style="text-align:left; white-space:nowrap; text-align:right;" width="20%">Jumlah</th>
+          <th style="text-align:left; white-space:nowrap; text-align:right;" width="25%">Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td style="text-align:left; white-space:nowrap;">Subaru Impreza</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">Rp 750.000</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">1 hari</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">Rp 750.000</td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap; padding-left: 20px;" colspan="4"><strong>Tahun:</strong> 2015</td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap; padding-left: 20px;" colspan="4"><strong>Transmisi:</strong> Manual</td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap; padding-left: 20px;" colspan="4"><strong>Bahan Bakar:</strong> Bensin</td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap;">Supir</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">Rp 220.000</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">1 hari</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">Rp 220.000</td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap;">BBM</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">Rp 400.000</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">-</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">Rp 400.000</td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap;" colspan="4"><strong>Ekstra</strong></td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap; padding-left: 20px;" colspan="2"><strong>+ GPS</strong></td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">1 unit</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;"><span style="font-size:9px; font-style:italic;">(bayar langsung ke rental)</span></td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap; padding-left: 20px;" colspan="2"><strong>+ Tempat duduk bayi</strong></td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;">0 unit</td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;"><span style="font-size:9px; font-style:italic;">(bayar langsung ke rental)</span></td>
+        </tr>
+        <tr>
+          <td style="text-align:left; white-space:nowrap; text-align:right;" colspan="3"><strong>Total</strong></td>
+          <td style="text-align:left; white-space:nowrap; text-align:right;"><strong>Rp 1.370.000</strong></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <div style="background-color:#fff; margin-bottom:1px; padding:20px; border-radius:0 0 3px 3px;">
+    <p style="font-size:14px; font-weight:bold;">Cara Pembayaran dan Rekening Tujuan</p>
+    <p>Silakan masukkan nomor order Anda dalam BERITA pada saat Anda melakukan pembayaran melalui ATM Non-Tunai, setoran Bank, atau Internet Banking.</p>
+    <p>Anda dapat melakukan pembayaran ke nomor rekening berikut :</p>
+    <p>
+      <strong>Bank BCA Kelapa Gading</strong> <br>
+      No. Rekening: <strong>125 009 504 5050</strong> <br>
+      Atas Nama: <strong>PT. Anugerah Gelindo</strong>
+    </p>
+      
+    <p style="margin:0 0 10px;">Kami menunggu pembayaran anda. Jika dalam 1x12 jam anda belum melakukan pembayaran, maka pesanan anda akan dibatalkan secara otomatis. Silahkan lakukan pembayaran dan konfirmasi dengan mengklik tombol di bawah ini. </p>
+    <p style="text-align:center; height:100px; line-height:100px;">
+      <a target="_blank" style="background:#d74c47;color:#fff;display:inline-block;font-size:18px;line-height:40px;text-align:center;text-decoration:none;width:230px" href="http://www.mobiltnt.com/payment-confirm">Konfirmasi Pembayaran</a>
+    </p>
+    <p style="margin:30px 0 10px;">Regards,</p>
+    <p style="margin:0 0 10px; font-weight:bold;">MobilTNT</p>  
+  </div>
+</div>
+<div style="font-size:11px; color:#b3b3b3; width:550px; margin:0 auto; text-align:center; margin-top:20px;">
+  Find Us on: <br/>
+  <a href="http://www.mobiltnt.com/" title="Facebook"><img style="width:40px;" src="http://www.mobiltnt.com/assets/img/fb.png"/></a> &nbsp; 
+  <a href="http://www.mobiltnt.com/" title="Twitter"><img style="width:40px;" src="http://www.mobiltnt.com/assets/img/tw.png"/></a> &nbsp; <br>
+  Copyright 2015 MobilTNT.com.<br/>
+  <a style="color:#fff; text-decoration:none;" href="http://www.mobiltnt.com/about-us.html"/>Tentang Kami</a> - 
+  <a style="color:#fff; text-decoration:none;" href="http://www.mobiltnt.com/contact-us.html"/>Kontak</a> - 
+  <a style="color:#fff; text-decoration:none;" href="http://www.mobiltnt.com/terms-condition.html"/>Syarat & Ketentuan</a>
+</div>
+</body>
+</html>
